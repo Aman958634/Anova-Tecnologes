7f3c8bf6b2060cee1d684a0127433f5b22b12ba0
Aman <amanullaathaniya@gmail.com>
2026-07-09 11:51:04 +0530

    google

--- Frontend/vite.config.mjs
+++ Frontend/vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
+  publicDir: 'public',
   plugins: [react()],
   build: {
     target: 'es2020',
